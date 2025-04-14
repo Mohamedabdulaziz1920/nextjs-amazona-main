26575896842af12b2e1c254150f867e488c8d775
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-04-14 20:34:53 +0300

    Update page.tsx

--- app/[locale]/(root)/account/manage/page.tsx
+++ app/[locale]/(root)/account/manage/page.tsx
@@ -1,8 +1,8 @@
 import { Metadata } from 'next'
 import { SessionProvider } from 'next-auth/react'
+import { getTranslations } from 'next-intl/server'
 
 import { auth } from '@/auth'
-
 import Link from 'next/link'
 import { Card, CardContent } from '@/components/ui/card'
 import { Separator } from '@/components/ui/separator'
@@ -12,27 +12,30 @@ const PAGE_TITLE = 'Login & Security'
 export const metadata: Metadata = {
   title: PAGE_TITLE,
 }
+
 export default async function ProfilePage() {
+  const t = await getTranslations('AccountPage')
   const session = await auth()
+
   return (
     <div className='mb-24'>
       <SessionProvider session={session}>
-        <div className='flex gap-2 '>
-          <Link href='/account'>Your Account</Link>
+        <div className='flex gap-2'>
+          <Link href='/account'>{t('BreadcrumbAccount')}</Link>
           <span>›</span>
-          <span>{PAGE_TITLE}</span>
+          <span>{t('LoginSecurity')}</span>
         </div>
-        <h1 className='h1-bold py-4'>{PAGE_TITLE}</h1>
-        <Card className='max-w-2xl '>
+        <h1 className='h1-bold py-4'>{t('LoginSecurity')}</h1>
+        <Card className='max-w-2xl'>
           <CardContent className='p-4 flex justify-between flex-wrap'>
             <div>
-              <h3 className='font-bold'>Name</h3>
+              <h3 className='font-bold'>{t('Name')}</h3>
               <p>{session?.user.name}</p>
             </div>
             <div>
               <Link href='/account/manage/name'>
                 <Button className='rounded-full w-32' variant='outline'>
-                  Edit
+                  {t('Edit')}
                 </Button>
               </Link>
             </div>
@@ -40,9 +43,9 @@ export default async function ProfilePage() {
           <Separator />
           <CardContent className='p-4 flex justify-between flex-wrap'>
             <div>
-              <h3 className='font-bold'>Email</h3>
+              <h3 className='font-bold'>{t('Email')}</h3>
               <p>{session?.user.email}</p>
-              <p>will be implemented in the next version</p>
+              <p>{t('ComingSoon')}</p>
             </div>
             <div>
               <Link href='#'>
@@ -51,7 +54,7 @@ export default async function ProfilePage() {
                   className='rounded-full w-32'
                   variant='outline'
                 >
-                  Edit
+                  {t('Edit')}
                 </Button>
               </Link>
             </div>
@@ -59,9 +62,9 @@ export default async function ProfilePage() {
           <Separator />
           <CardContent className='p-4 flex justify-between flex-wrap'>
             <div>
-              <h3 className='font-bold'>Password</h3>
-              <p>************</p>
-              <p>will be implemented in the next version</p>
+              <h3 className='font-bold'>{t('Password')}</h3>
+              <p>{t('HiddenPassword')}</p>
+              <p>{t('ComingSoon')}</p>
             </div>
             <div>
               <Link href='#'>
@@ -70,7 +73,7 @@ export default async function ProfilePage() {
                   className='rounded-full w-32'
                   variant='outline'
                 >
-                  Edit
+                  {t('Edit')}
                 </Button>
               </Link>
             </div>
