9e084b39aa40036f7e72c6acf646b4baa4b6241a
Mohamedabdulaziz1920 <140448813+Mohamedabdulaziz1920@users.noreply.github.com>
2025-04-14 16:58:07 +0300

    Update product-slider.tsx

--- components/shared/product/product-slider.tsx
+++ components/shared/product/product-slider.tsx
@@ -23,11 +23,11 @@ export default function ProductSlider({
         className={`
           grid 
           gap-4 
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
+          grid-cols-2
+          sm:grid-cols-3
+          md:grid-cols-4
+          lg:grid-cols-5
+          xl:grid-cols-6
         `}
       >
         {products.map((product) => (
